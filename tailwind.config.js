e75e457902c07783b1e165d181f232b34865b1ac
skrt-dev <jacobschwantes@outlook.com>
2022-02-04 00:37:29 -0600

    upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('tailwind-scrollbar-hide')
   ],
+  
 }
