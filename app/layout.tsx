a6986b38a399143472b9e18f70a0440343d48f09
Jacob <jacobschwantes@outlook.com>
2025-04-22 00:01:01 -0500

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -95,6 +96,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased dark`}
 			>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
